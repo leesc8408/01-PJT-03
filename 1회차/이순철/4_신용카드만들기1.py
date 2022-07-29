b4da7e0bf8b5617a79efc4ef8fab170539d9db8d
leesc8408 <leesc8408@gmail.com>
2022-07-29 17:38:11 +0900

    0729_모의고사 풀이중

--- 1회차/이순철/4_신용카드만들기1.py
+++ 1회차/이순철/4_신용카드만들기1.py
@@ -1,3 +1,19 @@
 import sys
 
 sys.stdin = open("_신용카드만들기1.txt")
+
+T = int(input())
+for test_case in range(1, T + 1):
+    card_ = list(map(int, input().split()))
+    # print(card_)
+    cnt = []
+    for i in range(len(card_)):
+        if i % 2 == 0:
+            cnt.append(card_[i] * 2)
+        else:
+            cnt.append(card_[i])
+    n = sum(cnt) % 10
+    if n == 0:
+        print(f'#{test_case} {n}')
+    else:
+        print(f'#{test_case} {10-n}')
--- 1회차/이순철/5_신용카드만들기2.py
+++ 1회차/이순철/5_신용카드만들기2.py
@@ -1,3 +1,10 @@
 import sys
 
 sys.stdin = open("_신용카드만들기2.txt")
+
+T = int(input())
+for test_case in range(1, T + 1):
+    card_no = input().split()
+    for i in card_no:
+        for j in i:
+            print(j)

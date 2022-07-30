42fc3b363875d995ee7fafa7206e0bcdc98065d5
leesc8408 <leesc8408@gmail.com>
2022-07-31 00:29:45 +0900

    0731_5번까지 풀이

--- 1회차/이순철/5_신용카드만들기2.py
+++ 1회차/이순철/5_신용카드만들기2.py
@@ -1,10 +1,13 @@
 import sys
 
 sys.stdin = open("_신용카드만들기2.txt")
-
+test =  ''
 T = int(input())
 for test_case in range(1, T + 1):
     card_no = input().split()
     for i in card_no:
-        for j in i:
-            print(j)
+        i = i.replace('-', '')
+        if len(i) == 16 and i[0] in '34569':
+            print(f'#{test_case} 1')
+        else:
+            print(f'#{test_case} 0') 
--- 1회차/이순철/6_암호문1.py
+++ 1회차/이순철/6_암호문1.py
@@ -1,3 +1,10 @@
 import sys
 
 sys.stdin = open("_암호문1.txt")
+
+# for test_case in range(1, 11):
+amho_len = int(input())
+amho = {}
+for i in range(amho_len):
+    amho.append(input().split())
+print(amho)

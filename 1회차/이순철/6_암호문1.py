2808dcbb34d530c095fbd99772ad196b89723271
leesc8408 <leesc8408@gmail.com>
2022-08-01 09:28:45 +0900

    모의고사 readme 추가입니다

--- 1회차/이순철/6_암호문1.py
+++ 1회차/이순철/6_암호문1.py
@@ -1,10 +1,14 @@
+from posixpath import split
 import sys
 
 sys.stdin = open("_암호문1.txt")
 
-# for test_case in range(1, 11):
-amho_len = int(input())
-amho = {}
-for i in range(amho_len):
-    amho.append(input().split())
-print(amho)
+for test_case in range(1, 11):
+    amho_len = int(input())
+    amho = input().split()
+    mi_len = int(input())
+    mi_list = list(input().split())
+    print(amho_len)
+    print(amho)
+    print(mi_len)
+    print(mi_list)
